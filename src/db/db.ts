147a0107bc637029a61616a4becb179d34885974
delanyo agbenyo <delanyodevio@gmail.com>
2025-07-23 02:37:00 +0000

    Feat: added better-auth

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-// import * as schema from "@src/db/schema";
+import * as schema from "@src/db/schema";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -8,4 +8,4 @@ export const pool = new Pool({
   idleTimeoutMillis: 30000,
 });
 
-export const db = drizzle(pool, { casing: "snake_case" });
+export const db = drizzle(pool, { casing: "snake_case", schema });
---
+++ src/lib/auth.ts
@@ -0,0 +1,8 @@
+import { db } from "@src/db/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: "pg" }),
+  emailAndPassword: { enabled: true },
+});
