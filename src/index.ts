5988719e1b39312953551c8d77a4a330ed466adb
delanyo agbenyo <delanyodevio@gmail.com>
2025-07-23 03:00:45 +0000

    Feat: added better-auth hono handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { auth } from "@src/lib/auth";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app
+  .on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw))
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  });
 
-export default app
+export default app;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
 import { db } from "@src/db/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg" }),
   emailAndPassword: { enabled: true },
+  plugins: [openAPI()],
 });
